0c18b99cb084be4012918cfac1310b8131d3fce5
Detsu | Mocho <36166146+Detsudetsu@users.noreply.github.com>
2021-04-30 02:53:17 +0900

    Hightlight one character icons

--- src/index.js
+++ src/index.js
@@ -1,1 +1,32 @@
 import "./styles.css";
+
+let highlightedImgName = "none";
+const imgs = document.getElementsByTagName("img");
+for (const img of imgs) {
+  img.addEventListener("click", () => {
+    const clickedImgName = img.getAttribute("src");
+    if (clickedImgName === highlightedImgName) {
+      resetHighlight();
+    } else {
+      highlight(clickedImgName);
+    }
+  });
+}
+
+function resetHighlight() {
+  for (const img of imgs) {
+    img.classList.remove("unselected");
+  }
+  highlightedImgName = "none";
+}
+
+function highlight(keySrcName) {
+  for (const img of imgs) {
+    if (img.getAttribute("src") === keySrcName) {
+      img.classList.remove("unselected");
+    } else {
+      img.classList.add("unselected");
+    }
+  }
+  highlightedImgName = keySrcName;
+}
